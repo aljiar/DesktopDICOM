0778af97c7200457d07cf31f0213598f7dca18bf
Alan Jimenez Arandia <aljiar_23@hotmail.com>
2019-03-26 10:30:00 -0400

    Adding more functionalities

---
+++ DesktopDICOM/ManagerAPP.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopDICOM
+{
+    class ManagerAPP
+    {
+        List<Paciente> listaPacientes;
+        List<Modalidad> listaModalidades;
+        List<SolictudModalidad> listaSolicitudes;
+        ServicioDICOM servicioDICOM = new ServicioDICOM();
+        ServicioTexto servicioTexto = new ServicioTexto();
+        public void AgregarPacientes(Paciente paciente)
+        {
+            listaPacientes.Add(paciente);
+        }
+        public void AgregarModalidad(Modalidad modalidad)
+        {
+            listaModalidades.Add(modalidad);
+        }
+        public void AgregarSolicitud(SolictudModalidad solictud)
+        {
+            listaSolicitudes.Add(solictud);
+        }
+        public void LevantarDICOM()
+        {
+            servicioDICOM.IniciarServidorDICOM();
+        }
+        public void AgregarADICOM()
+        {
+            servicioDICOM.AgregarArchivoDICOM();
+        }
+        public void ConsultaDICOM()
+        {
+            servicioDICOM.RealizarConsultaDICOM();
+        }
+        public void LeerArchivo(String ruta)
+        {
+            servicioTexto.LeerTexto(ruta);
+        }
+    }
+}
