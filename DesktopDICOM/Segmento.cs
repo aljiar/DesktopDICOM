ef81c3451eeb9136fe3d45c9188adde1f7ca4d6d
cae-1 <cae-1@LAPTOP-VQO7HG50>
2019-03-26 10:29:58 -0400

    Añadiendo admision de estudios

---
+++ DesktopDICOM/Segmento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesktopDICOM
+{
+    public class Segmento
+    {
+        // public Campo[] campos;
+        //public Campo[] camposobligados;
+
+        public List<Campo> campos;
+        public List<Campo> camposobligados;
+        public String errorText;
+        public String nombreSegmento;
+    }
+}
